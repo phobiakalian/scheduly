aecd4ae64fffa47322c2cfd2952f978fba402a1b
Khansa Gunawan <knsgnwn@Khansas-MacBook-Air.local>
2026-05-27 07:18:02 +0700

    fix: add complete android folder with manual gradlew for GitHub Actions

---
+++ android/app/src/main/java/com/scheduly/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.scheduly
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+  override fun getMainComponentName(): String = "Scheduly"
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
